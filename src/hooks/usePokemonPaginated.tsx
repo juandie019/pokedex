563a57375d5ad632684fe4066be30f5e46215b7a
Diego <jdiego.gomez@alumnos.udg.mx>
2023-04-10 12:46:26 -0600

    ADD: pokemons image displaye

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -37,5 +37,6 @@ export const usePokemonPaginated = () => {
     return {
         simplePokemonList,
         isLoading,
+        loadPokemons,
     };
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { ActivityIndicator, FlatList, Text } from 'react-native';
 import { Image } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { gstyles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
 
-  const { simplePokemonList } = usePokemonPaginated();
+  const { simplePokemonList, loadPokemons } = usePokemonPaginated();
 
   return (
     <>
@@ -16,10 +17,31 @@ export const HomeScreen = () => {
         source={ require('../assets/pokebola.png')}
         style={ gstyles.pokeBolaBg }
       />
-      <FlatList 
+      <FlatList
         data={ simplePokemonList }
         keyExtractor={ (pokemon) => pokemon.id }
-        renderItem={ ({item}) => <Text>{item.name}</Text>}
+        showsVerticalScrollIndicator={ false }
+        renderItem={ ({item}) => (
+          <FadeInImage
+            uri={ item.picture }
+            style={{
+              width: 100,
+              height: 100,
+            }}
+          />
+        )}
+
+        //infinite scroll
+        onEndReached={ () => loadPokemons() }
+        onEndReachedThreshold={ 0.4 }
+
+        ListFooterComponent={(
+          <ActivityIndicator
+            style={{ height: 100 }}
+            size={ 20 }
+            color="grey"
+          />
+        )}
       />
       {/* <Text style={{
         ...gstyles.title,
