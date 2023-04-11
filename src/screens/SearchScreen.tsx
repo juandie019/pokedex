8ce54cb5b4bd71f07d1e38ee83d39253457c3aa5
Diego <jdiego.gomez@alumnos.udg.mx>
2023-04-10 18:33:41 -0600

    ADD: Search Input

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Platform, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SearchInput } from '../components/SearchInput';
+import { gstyles } from '../theme/appTheme';
 
 export const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
   return (
-    <View>
-        <Text>SearchScreen</Text>
+    <View style={{
+        flex: 1,
+        top: (Platform.OS === 'ios') ? top : top + 10,
+        ...gstyles.globalMargin,
+      }}
+    >
+      <SearchInput />
     </View>
   );
 };
+
